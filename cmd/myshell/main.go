ee1c18f4303c146e0a71136a29ce62bd9b650767
João Pedro Resende Silva <90474257+notprQn@users.noreply.github.com>
2024-06-05 14:00:56 -0300

    codecrafters submit [skip ci]

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -2,17 +2,31 @@ package main
 
 import (
 	"bufio"
+
 	"fmt"
+
 	"os"
+
+	"strings"
 )
 
 func main() {
 	for {
 		fmt.Fprint(os.Stdout, "$ ")
+		cmd, err := bufio.NewReader(os.Stdin).ReadString('\n')
 
-		input, _ := bufio.NewReader(os.Stdin).ReadString('\n')
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
 
-		fmt.Fprint(os.Stdout, input[:len(input)-1]+": command not found\n")
+		cmd = strings.TrimSpace(cmd)
+
+		switch cmd {
+		case "exit 0":
+			os.Exit(0)
+		default:
+			fmt.Printf("%s: command not found\n", cmd)
+		}
 	}
-
 }
