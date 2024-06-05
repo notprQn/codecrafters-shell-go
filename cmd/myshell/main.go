35f08cd10719910c13be239fab88079c0a502148
João Pedro Resende Silva <90474257+notprQn@users.noreply.github.com>
2024-06-05 13:55:58 -0300

    codecrafters submit [skip ci]

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -7,9 +7,12 @@ import (
 )
 
 func main() {
-	fmt.Fprint(os.Stdout, "$ ")
+	for {
+		fmt.Fprint(os.Stdout, "$ ")
 
-	input, _ := bufio.NewReader(os.Stdin).ReadString('\n')
+		input, _ := bufio.NewReader(os.Stdin).ReadString('\n')
 
-	fmt.Fprint(os.Stdout, input[:len(input)-1]+": command not found\n")
+		fmt.Fprint(os.Stdout, input[:len(input)-1]+": command not found\n")
+	}
+
 }
